a364ef35add719896efc116a4def9aedf4983cfe
hanzsdev <149425914+hanzsdev@users.noreply.github.comm>
2024-01-12 17:21:42 +0000

    Add function for Player Choice w/ case insensitivity

--- rps.js
+++ rps.js
@@ -1,6 +1,7 @@
 // Setup random function for computer choice 
 // Declare variable to store random computer choice 
 // Setup function for player choice
+// Setup prompt to take player input
 // Declare variable to store player choice
 // Setup function taking input from computer and player choice
 // If statements comparing computer and player choice with outcome 
@@ -14,4 +15,30 @@ function getComputerChoice() {
     return randomOptions;
 }
 
-const computerChoice = getComputerChoice();
+const computerSelection = getComputerChoice();
+
+let playerChoice  =  (playerInput) => {
+    let rock = 'Rock';
+    let paper = 'Paper';
+    let scissors = 'Scissors';
+
+    if (playerInput.toLowerCase() === rock.toLowerCase()) {
+        playerChoice = 'Rock';
+    } else if (playerInput.toLowerCase() === paper.toLowerCase()) {
+        playerChoice = 'Paper';
+    } else  if (playerInput === scissors.toLowerCase()) {
+        playerChoice = 'Scissors';
+    };
+
+    return playerChoice;
+
+}
+
+playerChoice(prompt('Choose your hand',));
+
+playerSelection = playerChoice;
+console.log(playerSelection);
+
+
+
+
